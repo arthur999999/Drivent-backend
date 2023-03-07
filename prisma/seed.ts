174fa58db8e5813afea9a81eea105bee506d51c8
felipe <felipe.dreis.monteiro@gmail.com>
2023-03-06 21:33:44 -0300

    feat: dev seed with ticketTypes

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,5 @@
-import { PrismaClient } from "@prisma/client";
-import dayjs from "dayjs";
+import { PrismaClient } from '@prisma/client';
+import dayjs from 'dayjs';
 const prisma = new PrismaClient();
 
 async function main() {
@@ -7,16 +7,26 @@ async function main() {
   if (!event) {
     event = await prisma.event.create({
       data: {
-        title: "Driven.t",
-        logoImageUrl: "https://files.driveneducation.com.br/images/logo-rounded.png",
-        backgroundImageUrl: "linear-gradient(to right, #FA4098, #FFD77F)",
+        title: 'Driven.t',
+        logoImageUrl: 'https://files.driveneducation.com.br/images/logo-rounded.png',
+        backgroundImageUrl: 'linear-gradient(to right, #FA4098, #FFD77F)',
         startsAt: dayjs().toDate(),
-        endsAt: dayjs().add(21, "days").toDate(),
+        endsAt: dayjs().add(21, 'days').toDate(),
       },
     });
   }
-
-  console.log({ event });
+  let ticketType = await prisma.ticketType.findMany();
+  if (ticketType.length !== 3) {
+    await prisma.ticketType.createMany({
+      data: [
+        { name: 'type1', price: 100, isRemote: true, includesHotel: false},
+        { name: 'type2', price: 250, isRemote: false, includesHotel: false},
+        { name: 'type3', price: 600, isRemote: false, includesHotel: true},
+      ],
+    });
+  }
+  ticketType = await prisma.ticketType.findMany();
+  console.log({ event, ticketType });
 }
 
 main()
