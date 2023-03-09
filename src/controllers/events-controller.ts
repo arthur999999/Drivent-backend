d9e9900228f55b4d4acb2e1ec725d0b03a3b9e2f
arthur999999 <arthurhono.contato@gmail.com>
2023-03-09 01:02:13 -0300

    add redis to event route

--- src/controllers/events-controller.ts
+++ src/controllers/events-controller.ts
@@ -1,11 +1,18 @@
 import eventsService from "@/services/events-service";
+import client from "@/utils/redis";
 import { Request, Response } from "express";
 import httpStatus from "http-status";
 
 export async function getDefaultEvent(_req: Request, res: Response) {
   try {
-    const event = await eventsService.getFirstEvent();
-    return res.status(httpStatus.OK).send(event);
+    const  realEvent = await client.get("event");
+    if(realEvent) {
+      return res.status(httpStatus.OK).send(JSON.parse( realEvent ));
+    }else{
+      const event = await eventsService.getFirstEvent();
+      await client.set("event", JSON.stringify(event));
+      return res.status(httpStatus.OK).send(event);
+    }
   } catch (error) {
     return res.status(httpStatus.NOT_FOUND).send({});
   }
