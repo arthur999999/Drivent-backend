b17deeac37e1255dfb0625cdb6f492e7cf61b989
arthur999999 <arthurhono.contato@gmail.com>
2023-03-09 19:06:08 -0300

    add env on regis url

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -1,7 +1,7 @@
 import  { createClient } from "redis";
 
 const client = createClient({
-  url: "redis://:senha@localhost:6379"
+  url: process.env.REDIS_URL
 });
   
 client.on("error", err => console.log("Redis Client Error", err));
