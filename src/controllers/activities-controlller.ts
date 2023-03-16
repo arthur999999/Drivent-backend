0b5b4ba809cb38c8d990248400e6f39dfff91721
Lucas Ambar <lucass.ambar@gmail.com>
2023-03-16 19:45:30 -0300

    feat: get dates on actvities endpoint

--- src/controllers/activities-controlller.ts
+++ src/controllers/activities-controlller.ts
@@ -3,6 +3,18 @@ import activitiesService from "@/services/activities-service";
 import { Response } from "express";
 import httpStatus from "http-status";
 
+export async function getDates(req: AuthenticatedRequest, res: Response) {
+  try {
+    const { userId } = req;
+
+    const response = await activitiesService.getDates(userId);
+
+    res.send(response);
+  } catch (error) {
+    return res.sendStatus(httpStatus.NOT_FOUND);
+  } 
+}
+
 export async function getActivities(req: AuthenticatedRequest, res: Response) {
   try {
     const { userId } = req;
